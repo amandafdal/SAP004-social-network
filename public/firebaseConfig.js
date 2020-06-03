040e092d54ba759523f3d76c429a721aba0357f4
Zaine Nascimento <amandazaine@outlook.com>
2020-06-02 22:49:26 -0300

    Configurando Firebase

---
+++ public/firebaseConfig.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyB4F3KNbEFeCnv1d6co1uMamZ1RUO7XFaw",
+    authDomain: "social-network-sap004.firebaseapp.com",
+    databaseURL: "https://social-network-sap004.firebaseio.com",
+    projectId: "social-network-sap004",
+    storageBucket: "social-network-sap004.appspot.com",
+    messagingSenderId: "771744037701",
+    appId: "1:771744037701:web:0b03a163ee0fa2b3a35c2c",
+    measurementId: "G-NBY92F5JQY"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
