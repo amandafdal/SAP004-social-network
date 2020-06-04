6b994db3ecbb383f05fa95542b0b70f171791bc1
amandafdal <amandafdal@gmail.com>
2020-06-04 19:30:36 -0300

    ajustes de estilo

--- public/pages/login/main.js
+++ public/pages/login/main.js
@@ -8,7 +8,7 @@ export default () => {
             <input type="email" class="login-input"  id = "user-email" placeholder="Email">
             <input type="password" class="login-input" id="user-password" placeholder="Senha">
             <button class="login-btn" id="login-btn">Entrar</button>
-            <a class="register-link" href="#register">Cadastre-se</a>
+            <a class="create-acc register-link" href="#register">Cadastre-se</a>
         </div>
     `;
     container.innerHTML = template;
