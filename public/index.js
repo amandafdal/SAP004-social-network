dc32ac2bf2ce888486e08dc93af0fab0da0f104a
Amanda Manço <amand113@hotmail.com>
2020-06-04 12:43:08 -0300

    Integração SPAs

--- public/index.js
+++ public/index.js
@@ -1,12 +1,23 @@
-// Este é o ponto de entrada de sua aplicação
-//import { home } from './pages/home/main.js';
-//import routes from "./routes.js";
+import routes from "./routes.js";
 
+const main = document.querySelector("#root");
 
-window.addEventListener("load", () =>{
-    //document.querySelector('#root').appendChild(routes);
+const init = () => {
+    window.addEventListener("hashchange", () => {
+        renderPage()
+    })
+}
 
+const renderPage = () => {
+    main.innerHTML = "";
+    const page = validateHash(window.location.hash);
+    main.appendChild(routes[page]);
+}
+
+const validateHash = (hash) => hash === "" ? "login" : hash.replace("#", "");
+
+window.addEventListener("load", () => {
+    renderPage();
+    init();
 })
-
-
 
--- public/pages/login/main.js
+++ public/pages/login/main.js
@@ -3,13 +3,14 @@ export default () => {
     container.className = "login-page";
     const template = `
         <div>
-            <img src="img/LOGO-SH-SITE6.png" alt="Logo SafeHome"><img/>
+            <img src="img/LOGO-SH-SITE6.png" alt="Logo SafeHome">
         </div>
         <div class="login-container">
             <img src="img/LOGO-SH-SITE.png" alt="Safe House Logotype"><br>
             <input type="email" class="login-input"  id = "user-email" placeholder="Email"><br>
             <input type="password" class="login-input" id="user-password" placeholder="Senha"><br>
-            <button class="login" id="login-btn">Entrar</button>
+            <button class="login" id="login-btn">Entrar</button><br>
+            <a href="#register">Cadastre-se</a>
         </div>
     `;
     container.innerHTML = template;
