1e38b040951ac4462edf23e474a0956b7e97088d
amandafdal <amandafdal@gmail.com>
2020-06-03 21:35:30 -0300

    style login and register pages

--- public/pages/login/main.js
+++ public/pages/login/main.js
@@ -2,14 +2,13 @@ export default () => {
     const container = document.createElement('div');
     container.className = "login-page";
     const template = `
-        <div>
-            <img src="img/LOGO-SH-SITE6.png" alt="Logo SafeHome"><img/>
-        </div>
+        <img class="logo" src="img/LOGO-SH-SITE6.png" alt="Logo SafeHome"><img/>
         <div class="login-container">
-            <img src="img/LOGO-SH-SITE.png" alt="Safe House Logotype"><br>
-            <input type="email" class="login-input"  id = "user-email" placeholder="Email"><br>
-            <input type="password" class="login-input" id="user-password" placeholder="Senha"><br>
-            <button class="login" id="login-btn">Entrar</button>
+            <img class="logotype" src="img/LOGO-SH-SITE.png" alt="Safe House Logotype">
+            <input type="email" class="login-input"  id = "user-email" placeholder="Email">
+            <input type="password" class="login-input" id="user-password" placeholder="Senha">
+            <button class="login-btn" id="login-btn">Entrar</button>
+            <p class="create-acc">Não tem uma conta? <a class="register-link" href="/#register">Registre-se</a></p>
         </div>
     `;
     container.innerHTML = template;
