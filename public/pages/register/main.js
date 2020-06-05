922736c497e8b6935f772f3198378e870c830cc1
Amanda Fernandes <amandafdal@gmail.com>
2020-06-05 13:42:53 -0300

    Update main.js

--- public/pages/register/main.js
+++ public/pages/register/main.js
@@ -2,17 +2,13 @@ export default () =>{
     const container = document.createElement("div");
     container.className = "login-page";
     const template = `
-        
-        <img src="img/LOGO-SH-SITE6.png" alt="Safe Home Logo">
-        
+        <img class="logo" src="img/LOGO-SH-SITE6.png" alt="Safe Home Logo">
         <div class="login-container">
             <img class="logotype" src="img/LOGO-SH-SITE.png" alt="Safe Home Logotype">
             <input type="text" class="login-input" id = "user-name" placeholder="Nome">
             <input type="email" class="login-input" id = "user-email" placeholder="Email">
             <input type="password" class="login-input" id = "user-password" placeholder="Senha">
             <input type="password" class="login-input" id = "user-password-confirm" placeholder="Confirme sua senha">
-            <button class="login-btn" id="create-account-btn">Criar conta</button>
-
             <button class="login" id="create-account-btn" href="#home">Criar conta</button>
             <p class="create-acc">Já possui conta?
                 <a class="register-link" href="#login">Faça o seu login</a>  
