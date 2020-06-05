62d32a6052b4bbfe1dc69052d9a211a0d7eb7e76
Amanda Fernandes <amandafdal@gmail.com>
2020-06-05 13:48:08 -0300

    Update main.js

--- public/pages/login/main.js
+++ public/pages/login/main.js
@@ -2,9 +2,9 @@ export default () => {
     const container = document.createElement('div');
     container.className = "login-page";
     const template = `
-        <img src="img/LOGO-SH-SITE6.png" alt="Logo SafeHome">
+        <img class="logo" src="img/LOGO-SH-SITE6.png" alt="Logo SafeHome">
         <div class="login-container">
-            <img src="img/LOGO-SH-SITE.png" alt="Safe House Logotype">
+            <img class="logotype" src="img/LOGO-SH-SITE.png" alt="Safe House Logotype">
             <input type="email" class="login-input"  id = "user-email" placeholder="Email">
             <input type="password" class="login-input" id="user-password" placeholder="Senha">
             <button class="login-btn" id="login-btn">Entrar</button>
@@ -14,4 +14,3 @@ export default () => {
     container.innerHTML = template;
     return container;
 }
-
