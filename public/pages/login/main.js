f4f591f86fe0e6dd28c4807c8255c4791f62e67e
Zaine Nascimento <amandazaine@outlook.com>
2020-06-10 13:42:12 -0300

    Tentativa de merge2

--- public/pages/login/main.js
+++ public/pages/login/main.js
@@ -12,21 +12,6 @@ export default () => {
         </div>
     `;
     container.innerHTML = template;
-<<<<<<< HEAD
-
-    container.querySelector("#login-btn").addEventListener("click", (event) =>{
-        event.preventDefault();
-        
-        const email = document.getElementById("user-email").value; 
-        const password = document.getElementById("user-password").value;
-                
-        firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
-            alert("Você está logado com email e senha!");
-          });
-        
-    })
-    
-=======
     
     container.querySelector("#login-btn").addEventListener("click", (event) =>{
         event.preventDefault();
@@ -37,6 +22,5 @@ export default () => {
                 window.location.hash = "home"
         });
     });
->>>>>>> e21dbe28d4e1f2b788106352f5a21e8735cf8aeb
     return container;
 }
